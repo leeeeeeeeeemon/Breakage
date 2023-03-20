0840a5e37ce5d442a1e329e254c357bf02e79361
leeeeeeeeeemon <emilbarish@gmail.com>
2023-03-20 11:43:21 +0300

    Добавьте файлы проекта.

---
+++ Breakage/Data/BreakageMishaEntities.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Breakage.Data
+{
+    public partial class BreakageMishaEntities
+    {
+        private static polomkaEntities _context;
+        public static polomkaEntities GetContext()
+        {
+            if( _context == null )
+                _context = new polomkaEntities();
+
+            return _context;
+        }
+    }
+}
